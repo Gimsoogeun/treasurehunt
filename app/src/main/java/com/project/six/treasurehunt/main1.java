08ddbc909b014f0ae54b28e9503065c6dabda361
차건희 <gunheel123@nate.com>
2017-11-14 21:25:01 +0900

    제대로 깃헙에 commit 되는지 확인합니다

--- app/src/main/java/com/project/six/treasurehunt/main1.java
+++ app/src/main/java/com/project/six/treasurehunt/main1.java
@@ -2,14 +2,41 @@ package com.project.six.treasurehunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class main1 extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+public class main1 extends AppCompatActivity {
+    DatabaseReference mDatabase= FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef=mDatabase.child("name");
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
         Toast.makeText(this, "commit test", Toast.LENGTH_SHORT).show();
+        textView=(TextView) findViewById(R.id.textview);
     }
+    protected void onStart(){
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name=dataSnapshot.getValue(String.class);
+                textView.setText(name);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
